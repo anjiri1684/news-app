d6a48edf25cae44bccf91e7dde5dd58d39678c18
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-20 14:34:39 +0300

    "Remove page.js files from app/archive/@archive/[year] and app/archive/@archive"

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,67 @@
+import NewsList from "@/components/news-list";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
+import Link from "next/link";
+
+export default function FIlteredNewsPage({ params }) {
+  const filter = params.filter;
+
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContents = <p>No News found for the selected period</p>;
+
+  if (news && news.length > 0) {
+    newsContents = <NewsList news={news} />;
+  }
+
+  if (
+    (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+    (selectedMonth &&
+      !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+  ) {
+    throw new Error("Invalid filter");
+  }
+  return (
+    <>
+      {" "}
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContents}
+    </>
+  );
+
+  // const news = getNewsForYear(newsYear);
+  // return <NewsList news={news} />;
+}
--- app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import NewsList from "@/components/news-list";
-import { getNewsForYear } from "@/lib/news";
-
-export default function FIlteredNewsPage({ params }) {
-  const newsYear = params.year;
-  const news = getNewsForYear(newsYear);
-  return <NewsList news={news} />;
-}
